f773910fa827476378242e976aeb5112d487b95f
Bruce Stockwell <bruce.stockwell@gmail.com>
2018-08-25 23:20:38 -0400

    refactored fo readability

--- src/lib/Button/index.js
+++ src/lib/Button/index.js
@@ -3,27 +3,34 @@ import styled from "styled-components";
 import * as colors from "../colors";
 import { spacing } from "../spacing";
 
-const hoverColor = disabled =>
-  !disabled
+const hoverColor = disabled => (!disabled ? staticColor(true) : null);
+
+const staticColor = primary =>
+  primary
     ? `
         background: ${colors.PRIMARY};
         color: ${colors.SECONDARY};
         `
-    : null;
+    : `
+        background: ${colors.SECONDARY};
+        color: ${colors.PRIMARY};
+        `;
+
+const border = () => `
+    border-width: ${spacing(1)};
+    border-style: solid;
+    border-color: ${colors.PRIMARY};`;
 
 const StyledButton = styled.button`
   padding-left: ${spacing(4)};
   padding-right: ${spacing(4)};
   padding-top: ${spacing(2)};
   padding-bottom: ${spacing(2)};
-  background: ${({ primary }) => (primary ? colors.PRIMARY : colors.SECONDARY)};
-  color: ${({ primary }) => (primary ? colors.SECONDARY : colors.PRIMARY)};
-  border-width: ${spacing(1)};
+  ${({ primary }) => staticColor(primary)};
+  ${border()};
   &:disabled {
     opacity: 0.3;
   }
-  border-style: solid;
-  border-color: ${colors.PRIMARY};
   &:hover {
     ${({ disabled }) => hoverColor(disabled)};
   }
