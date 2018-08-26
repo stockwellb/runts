5e455fc961d07cbc9c6d40e5f44523dcbf15260c
Bruce Stockwell <bruce.stockwell@gmail.com>
2018-08-25 23:30:21 -0400

    reorg stories

--- src/lib/Button/Button.stories.js
+++ src/lib/Button/Button.stories.js
@@ -4,10 +4,24 @@ import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 import { Button } from "./index";
+import { UpperCase } from "../index";
 
 storiesOf("Button", module)
-.add("Default", () => <Button>Save</Button>)
-.add("Primary Disabled", () => <Button disabled primary >Save</Button>)
-.add("Disabled", () => <Button disabled >Save</Button>)
-.add("Primary", () => <Button primary >Save</Button>)
-.add("onClick", () => <Button primary onClick={action("button clicked")} >Save</Button>)
+  .add("Default", () => <Button>Save</Button>)
+  .add("Default disabled", () => <Button disabled>Save</Button>)
+  .add("Primary", () => <Button primary>Save</Button>)
+  .add("Primary disabled", () => (
+    <Button disabled primary>
+      Save
+    </Button>
+  ))
+  .add("Primary upperCased", () => (
+    <Button primary onClick={action("button clicked")}>
+      <UpperCase>Save</UpperCase>
+    </Button>
+  ))
+  .add("OnClick", () => (
+    <Button primary onClick={action("button clicked")}>
+      Save
+    </Button>
+  ));
